a980231207fde2a68fdd328417745f72dc137262
organ <organnet@mail.ru>
2023-02-22 03:25:02 +0300

    Creating a interface PersonRepository.java

---
+++ src/main/java/com/example/springsecurity/repository/PersonRepository.java
@@ -0,0 +1,18 @@
+package com.example.springsecurity.repository;
+
+import com.example.springsecurity.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    List<Person> findByCityOfLiving(String city);
+
+    List<Person> findByAgeLessThanOrderByAge(int age);
+
+    Optional<Person> findByNameAndSurname(String name, String surname);
+}
